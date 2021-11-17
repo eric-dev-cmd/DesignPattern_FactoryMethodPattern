571f587186b01a0b4c1d855da8c21abe3aa6cf7c
zunggzing <hello.trungvinh@gmail.com>
2021-11-17 13:21:31 +0700

    Test branch

--- src/test/TestFactory.java
+++ src/test/TestFactory.java
@@ -9,7 +9,7 @@ public class TestFactory {
         Car honda = CarFactory.getCar("honda", "Name Honda 1", 200000, "Config Perfect 1", "Honda Jpan", "Feature 1");
         Car nexus = CarFactory.getCar("nexus", "Name Nexus 2", 300000.22, "Config Perfect 2", "Nexus Jpan", "Feature 2");
         Car toyota = CarFactory.getCar("toyota", "Name Toyota 3", 400.20000, "Config Perfect 3", "Toyota Jpan", "Feature 3");
-
+        System.out.println("Bank");
         System.out.println("Factory Honda Config::"+honda);
         System.out.println("Factory Nexus Config::"+nexus);
         System.out.println("Factory Toyota Config::"+toyota);
