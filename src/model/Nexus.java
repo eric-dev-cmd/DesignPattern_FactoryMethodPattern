a03eccf28a24d6fe894602daf46e5413a27abc2e
zunggzing <hello.trungvinh@gmail.com>
2021-11-17 12:02:08 +0700

    Add branch Car && finished

---
+++ src/model/Nexus.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Nexus extends  Car{
+    private String name;
+    private double price;
+    private String config;
+    private String producer;
+    private String feature;
+
+    public Nexus(String name, double price, String config, String producer, String feature) {
+        this.name = name;
+        this.price = price;
+        this.config = config;
+        this.producer = producer;
+        this.feature = feature;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String getConfig() {
+        return this.config;
+    }
+
+    @Override
+    public String getProducer() {
+        return this.producer;
+    }
+
+    @Override
+    public String getFeature() {
+        return this.feature;
+    }
+}
--- src/test/TestFactory.java
+++ src/test/TestFactory.java
@@ -1,14 +1,18 @@
 package test;
 
-import factory.ComputerFactory;
-import model.Computer;
+import factory.CarFactory;
+import model.Car;
 
 public class TestFactory {
     public static void main(String[] args) {
-        Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
-        Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
-        System.out.println("Factory PC Config::"+pc);
-        System.out.println("Factory Server Config::"+server);
+        System.out.println("===== Car =====");
+        Car honda = CarFactory.getCar("honda", "Name Honda 1", 200000, "Config Perfect 1", "Honda Jpan", "Feature 1");
+        Car nexus = CarFactory.getCar("nexus", "Name Nexus 2", 300000.22, "Config Perfect 2", "Nexus Jpan", "Feature 2");
+        Car toyota = CarFactory.getCar("toyota", "Name Toyota 3", 400.20000, "Config Perfect 3", "Toyota Jpan", "Feature 3");
+
+        System.out.println("Factory Honda Config::"+honda);
+        System.out.println("Factory Nexus Config::"+nexus);
+        System.out.println("Factory Toyota Config::"+toyota);
     }
 
 }
